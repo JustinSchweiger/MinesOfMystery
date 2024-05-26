31edb36914d8afc2cab43aa43c87cb63e4a917f9
Justin <Justin.Schweiger@t-online.de>
2024-05-26 22:09:36 +0200

    Init

---
+++ src/main/java/eu/playsc/minesofmystery/MinesOfMystery.java
@@ -0,0 +1,19 @@
+package eu.playsc.minesofmystery;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinesOfMystery extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+
+
+    @Override
+    public void onDisable() {
+
+    }
+}
